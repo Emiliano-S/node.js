1436d15a774d4d4d0cc89f79a3cd0dd19f3daf0d
Emiliano G. Scanga <scangaemiliano@gmail.com>
2022-10-06 19:50:37 +0200

    End ex17

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,39 @@
-import express from 'express';
-import 'express-async-errors';
+import express from "express";
+import "express-async-errors";
+
+import prisma from "./lib/prisma/client";
+
+import {
+  validate,
+  validationErrorMiddleware,
+  planetSchema,
+  PlanetData,
+} from "./lib/validation";
 
 const app = express();
-app.use(express.json())
 
-app.get('/planets', (request, response) => {
-    response.json([
-        {name: "Mercury"},
-        {name: "Venus"}
-    ]);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("This is the Space Facts API!");
 });
 
-app.post('/planets', (request, response) =>{
-    const planet = request.body;
+app.get("/planets", async (req, res) => {
+  const planets = await prisma.planet.findMany();
 
-    response.status(201).json(planet);
-})
+  res.json(planets);
+});
 
+app.post("/planets", validate({ body: planetSchema }), async (req, res) => {
+  const planetData: PlanetData = req.body;
+
+  const planet = await prisma.planet.create({
+    data: planetData,
+  });
+
+  res.status(201).json(planet);
+});
+
+app.use(validationErrorMiddleware);
 
 export default app;
